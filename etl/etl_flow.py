b3f5dfd53908ac9e6921957703ffdf9eed2da274
Danilo Pinto da Silva <danilobits@gmail.com>
2025-10-11 23:03:02 -0300

    Add command to rebuild container for environment variable refresh

--- etl/etl_flow.py
+++ etl/etl_flow.py
@@ -10,6 +10,9 @@ docker exec -w /app/dbt saas_hiremetrics_etl dbt deps
 docker exec saas_hiremetrics_etl dbt run --project-dir dbt --profiles-dir dbt
 docker exec saas_hiremetrics_etl python scripts/copy_job_dashboard_base.py
 docker exec saas_hiremetrics_etl python etl_flow.py
+
+- rebuild container (force refresh env. variables)
+docker-compose up -d --build hiremetrics_elt
 """
 
 import logging
